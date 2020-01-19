2c5d18600960edfc1ac0d04ff3a1ad08611fe793
zhaodi <zzdd@sjtu.edu.cn>
2020-01-19 22:55:43 +0800

    commit

--- src/main/java/com/example/nliot/Constants.java
+++ src/main/java/com/example/nliot/Constants.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Constants {
-    public static final String parentAddress = "上海市闵行区东川路800号软件学院";
-    public static final String physicalAddress = "上海市闵行区东川路800号软件学院3503";
-    public static final String ipAddress = "192.168.0.149";
-    public static final String tempServerName = "server1";
-    public static final String tempClientName = "client1";
-    public static final String mqttUrl = "tcp://0.0.0.0:1883";
+    public static final String parentAddress = "上海市闵行区东川路800号";
+    public static final String physicalAddress = "上海市闵行区东川路800号软件学院";
+    public static final String ipAddress = "192.168.1.103";
+    public static final String tempServerName = "server2";
+    public static final String tempClientName = "client2";
+    public static final String mqttUrl = "tcp://192.168.1.101:1883";
 }
